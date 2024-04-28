839b40f43ebb193f105e2768546e87bec32757e0
Vaishnavih988 <167509468+Vaishnavih988@users.noreply.github.com>
2024-04-28 22:40:59 +0530

    Add files via upload

---
+++ list2.py
@@ -0,0 +1,10 @@
+orange=[10,'balaji',250000.75,'z']
+print(orange)
+
+for i in orange:
+    print(i,end="-")
+print()
+    
+pineapple=[250000.75,'balaji',10,'z']
+print(pineapple)
+print(pineapple==orange)
